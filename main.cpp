3d4b82fd8bb5f45057084aa6780fd09f13160731
xar8 <jevans42@ecc-jaguar.csuchico.edu>
2018-09-26 17:15:00 -0700

    Added loop

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author: Jason Evans
 #include<iostream>
 #include<string>
 using namespace std;
@@ -8,12 +9,14 @@ int main()
   int numItems = 0;
   char input;
 
+  do 
+  {
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
-
+  } while (input != 'q' && input != 'Q'); 
   return 0;
 }
